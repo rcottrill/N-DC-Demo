6efaa0e66079f23e32f1a1e921d562924cb554e9
Rob Cottrill <rcottrill@ACAC0538.ipt.aol.com>
2018-10-29 10:10:16 +0000

    Initial

---
+++ DSC/ConfigureADServers/ConfigureADServers.ps1
@@ -0,0 +1,219 @@
+﻿configuration ConfigureADServers
+{
+   param
+   (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [String]$DC02IP,
+
+        [Parameter(Mandatory)]
+        [String]$DC01IP,
+
+        [Parameter(Mandatory)]
+        [String]$SiteCIDR,
+
+        [Parameter(Mandatory)]
+        [String]$VMRole,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+
+
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+   
+   Import-DscResource -ModuleName xActiveDirectory, xNetworking, xPendingReboot
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node $AllNodes.Where{$VMRole -eq "FirstDC"}.Nodename
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+
+        Script EnableDNSDiags
+        {
+      	    SetScript = {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DC01IP, $DC02IP
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn="[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+
+        xADDomain DC1
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "C:\Windows\NTDS"
+            LogPath = "C:\Windows\NTDS"
+            SysvolPath = "C:\Windows\SYSVOL"
+            DependsOn = @("[WindowsFeature]ADDSInstall")
+        }
+
+           Script UpdateDNSForwarder
+        {
+            SetScript =
+            {
+                Write-Verbose -Verbose "Getting DNS forwarding rule..."
+                Add-DnsServerForwarder -IPAddress '8.8.8.8' -PassThru
+                Add-DnsServerForwarder -IPAddress '8.8.4.4' -PassThru
+                Write-Verbose -Verbose "End of UpdateDNSForwarder script..."
+            }
+            GetScript =  { @{} }
+            TestScript = {$false}
+            DependsOn = "[xADDomain]DC1"
+        }
+
+        xADReplicationSite DefaultSite{
+            Name = "Azure"
+            Ensure = "Present"
+            RenameDefaultFirstSiteName = $true
+            DependsOn = "[xADDomain]DC1"
+        }
+
+         xADReplicationSubnet Azure{
+            Name = $SiteCIDR
+            Site = "Azure"
+            Ensure = "Present"
+            DependsOn = "[xADReplicationSite]DefaultSite"
+        }
+
+        xPendingReboot RebootAfterPromotion{
+            Name = "RebootAfterPromotion"
+            DependsOn = "[xADReplicationSubnet]Azure"
+        }
+
+   }
+
+   Node $AllNodes.Where{$VMRole -eq "DCReplica"}.Nodename
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DC01IP, $DC02IP
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn="[WindowsFeature]ADDSInstall"
+        }
+
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+        }
+
+        xADDomainController DC2
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "C:\Windows\NTDS"
+            LogPath = "C:\Windows\NTDS"
+            SysvolPath = "C:\Windows\SYSVOL"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        Script UpdateDNSForwarder
+        {
+            SetScript =
+            {
+                Write-Verbose -Verbose "Getting DNS forwarding rule..."
+                Add-DnsServerForwarder -IPAddress '8.8.8.8' -PassThru
+                Add-DnsServerForwarder -IPAddress '8.8.4.4' -PassThru
+                Write-Verbose -Verbose "End of UpdateDNSForwarder script..."
+            }
+            GetScript =  { @{} }
+            TestScript = {$false}
+            DependsOn = "[xADDomainController]DC2"
+        }
+
+        xPendingReboot RebootAfterPromotion {
+            Name = "RebootAfterDCPromotion"
+            DependsOn = "[xADDomainController]DC2"
+        }
+
+
+    }
+
+}
